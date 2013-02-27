bdfbfe6d1cade16499254ad26d5c15062e86884d
Guillaume Harvey <guilh_22@hotmail.com>
2013-02-27 16:14:54 -0500

    Ajout de ajouterDescription dans class Film, création du ficheir TupleFilm ainsi qu'implementation de certaine méthode de GestionFilm

--- tp3/Film.java
+++ tp3/Film.java
@@ -27,4 +27,16 @@ class Film {
     void ajouter(String titre, Date dateSortie, String realisateur) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    void enlever(String titre, Date dateSortie) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    TupleFilm getFilm(String titre, Date dateSortie){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void ajouterDescription(TupleFilm film, String description, int duree) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
