46fbea508a16e596d12bca2df4caa79aff5fd3f8
Mathieu Larocque <math.larocque@gmail.com>
2013-02-27 16:40:24 -0500

    gestionRoleEpisode et gestionRoleFilm

--- tp3/GestionRoleEpisode.java
+++ tp3/GestionRoleEpisode.java
@@ -10,17 +10,51 @@ import java.sql.Date;
  *
  * @author guillaume
  */
-class GestionRoleEpisode {
+class GestionRoleEpisode  {
+    
+    
+    private Serie serie;
+    private Episode episode;
+    private Personne personne;
+    private RoleEpisode roleEpisode;
+    private Connexion cx;
 
-    GestionRoleEpisode(RoleEpisode roleEpisode, Serie serie, Personne personne) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    GestionRoleEpisode(RoleEpisode re, Serie s, Personne p) 
+        throws Tp3Exception{
+        this.serie = s;
+        this.episode = e;
+        this.personne = p;
+        this.roleEpisode = re;
+        if(serie.getConnection() != episode.getConnection() && 
+           serie.getConnection() != personne.getConnection() && 
+           serie.getConnection() != roleEpisode.getConnection()){
+            throw new Tp3Exception("Connection non valide");
+        }
+        this.cx = s.getConnection();
     }    
 
-    void ajoutRole(String readString, Date readDate, String readString0, String readString1, String readString2, String readString3) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    void ajoutRole(String readString, Date readDate, int readInt, int readInt0, String readString0, String readString1) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    void ajoutRole(String serieTitre, Date serieDate, int noSaison, int noEpisode, String acteur, String roleActeur) 
+        throws Tp3Exception{
+        if(serie.existe(serieTitre, serieDate )){
+            if(episode.existe(serieTitre, serieDate, noSaison, noEpisode)){
+                if(personne.existe(acteur)){
+                    TuplePersonne tupleActeur = personne.getPersonne(acteur);
+                    if(tupleActeur.getDateNaissance().before(serieDate)){
+                        if(!roleEpisode.existe(serieTitre, serieDate, noSaison, noEpisode, acteur, roleActeur)){
+                            roleEpisode.ajouter(serieTitre, serieDate, noSaison, noEpisode, acteur, roleActeur);
+                        }
+                    }else{
+                        throw new Tp3Exception("L'acteur : " + acteur + " joue deja dans l'episode " +
+                                noEpisode + " de la saison " + noSaison + " dans le role de : " + roleActeur);
+                    }
+                }else{
+                    throw new Tp3Exception("L'acteur : " + acteur + " n'existe pas.");
+                }
+            }else{
+                throw new Tp3Exception("L'episode no : " + noEpisode + " de la saison : " + noSaison + " n'existe pas.");
+            }
+        }else{
+            throw new Tp3Exception("La serie : " + serieTitre + " n'existe pas.");
+        }
     }
 }
--- tp3/GestionRoleFilm.java
+++ tp3/GestionRoleFilm.java
@@ -5,26 +5,93 @@
 package tp3;
 
 import java.sql.Date;
+import java.util.List;
 
 /**
  *
  * @author guillaume
+ * @author mathieu
  */
 class GestionRoleFilm {
-
-    GestionRoleFilm(RoleFilm roleFilm, Film film, Personne personne) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    
+    private Film film;
+    private Personne personne;
+    private RoleFilm roleFilm;
+    private Connexion cx;
+    
+    GestionRoleFilm(RoleFilm rf, Film f, Personne p) {
+        
+        this.film = f;
+        this.personne = p;
+        this.roleFilm = rf;
+        if(film.getConnection() != personne.getConnection() && 
+           film.getConnection() != roleFilm.getConnection()){
+            throw new Tp3Exception("Connection non valide");
+        }
+        this.cx = f.getConnection();
     } 
 
-    void ajoutActeurFilm(String readString, Date readDate, String readString0, String readString1) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    void ajoutActeurFilm(String titre, Date anneeSortie, String acteur, String role) 
+            throws Tp3Exception{
+        
+        if (!Main.isStringNotEmpty(titre) || !Main.isStringNotEmpty(acteur)
+                || !Main.isStringNotEmpty(role)) {
+            //une donner est invalide
+            throw new Tp3Exception("Un parametre est invalide (titre = '"
+                    + titre + "', date de sortie = '" + anneeSortie
+                    + "', acteur = '" + acteur + "', role = '" + role + "').\n");
+        } else {
+            if (personne.existe(acteur)) {
+                if (film.existe(titre, anneeSortie)) {
+                    TuplePersonne acteur = personne.getPersonne(acteur);
+                    if(acteur.getDateNaissance().before(anneeSortie)){
+                        if (!roleFilm.existe(acteur, titre, anneeSortie)) {
+                            roleFilm.ajouter(acteur, titre, anneeSortie);
+                            cx.commit();
+                        } else {
+                            throw new Tp3Exception(
+                                    "Impossible d'ajouter l'acteur au film puisque que celui-ci y joue deja.\n");
+                        }
+                    }else{
+                        throw new Tp3Exception(
+                                    "L'acteur semble etre nee avant la date de sortie du film.\n");
+                    }
+                } else {
+                    throw new Tp3Exception(
+                            "Impossible d'ajouter l'acteur au film puisque que le film n'existe pas.\n");
+                }
+            } else {
+                throw new Tp3Exception(
+                        "Impossible d'ajouter l'acteur au film puisque que l'acteur n'existe pas.\n");
+            }
+        }
     }
 
-    void getActeurFromFilm(String readString, Date readDate) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    void getActeurFromFilm(String titre, Date anneeSortie) throws Tp3Exception{
+        if(film.existe(titre, anneeSortie)){
+            List <TupleRoleFilm> tuples = roleFilm.getActeurs(titre, anneeSortie);
+            String output = "";
+            while(!tuples.empty()){
+                output += tuples.front().getNom() + (tuples.size() > 1)?", ":"";
+                tuples.pop_front();
+            }
+            System.out.println(output);
+        }else{
+            throw new Tp3Exception("Le film : " + titre + ", " + anneeSortie + " n'existe pas.");
+        }
     }
 
-    void getFilmFromActeur(String readString) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    void getFilmFromActeur(String nom) throws Tp3Exception{
+        if(personne.existe(nom)){
+            list<TupleRoleFilm> tuples = roleFilm.getFilmWhereActeur(nom); 
+            string output = "";
+            while(!tuples.empty()){
+                output += tuples.front().getTitre() + (tuples.size() > 1)?", ":"";
+                tuples.pop_front();
+            }
+            System.out.println(output);
+        }else{
+            throw new Tp3Exception("L'acteur " + nom + " n'existe pas.");
+        }
     }
 }
